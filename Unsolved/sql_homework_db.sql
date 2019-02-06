2d402b7e2dcb312ffd8c5b143d7cdb24e17dd42d
leetaylor7 <ltaylor@goodmanfinancial.com>
2019-02-06 16:58:07 -0600

    before leaving

--- Unsolved/sql_homework_db.sql
+++ Unsolved/sql_homework_db.sql
@@ -1,56 +1,72 @@
 USE sakila;
 
-SELECT first_name FROM actor;
-
+#This answers 1a
 SELECT first_name, last_name FROM actor;
 
+#This Answers 1b
+SELECT CONCAT(first_name, ' ', last_name) FROM actor AS full_name;
+
+#This Answers 2a
 SELECT actor_id, first_name, last_name FROM actor
 	WHERE first_name = "Joe";
-    
+
+#This Answers 2b
 SELECT * FROM actor
 	WHERE last_name LIKE "%gen%";
-    
+ 
+#This Answers 2c 
 SELECT * FROM actor
 	WHERE last_name LIKE "%li%"
     ORDER BY last_name, first_name ASC;
     
-
+#This Answers 2d 
 SELECT country_id, country
 FROM country
 WHERE country IN ("Afghanistan", "Bangladesh", "China");
 
+#This Answers 3a 
 ALTER TABLE actor
 ADD description BLOB;
 
+#This Answers 3b
 ALTER TABLE actor
 DROP COLUMN description;
 
+
+#This Answers 4a
 SELECT  last_name, COUNT(last_name)
 FROM actor
 GROUP BY last_name
 ORDER BY COUNT(last_name) DESC;
 
+#This Answers 4b
 SELECT  last_name, COUNT(last_name)
 FROM actor
 GROUP BY last_name
 HAVING COUNT(last_name) >= 2
 ORDER BY COUNT(last_name) DESC;
 
+
+#This Answers 4c
 UPDATE actor
 SET first_name = "HARPO"
 WHERE first_name = "GROUCHO" AND last_name = "WILLIAMS";
 
+#This Answers 4d
 UPDATE actor
 SET first_name = "GROUCHO"
 WHERE first_name = "HARPO" AND last_name = "WILLIAMS";
 
+#This Answers 5a
 SHOW CREATE TABLE address;
 
+#This Answers 6a
 SELECT staff.first_name, staff.last_name, address.address
 FROM address
 INNER JOIN staff ON
 staff.address_id=address.address_id;
 
+#This Answers 6b
 SELECT staff.staff_id, staff.first_name, staff.last_name, payment.mySUM
 FROM  staff
 INNER JOIN 
@@ -59,12 +75,46 @@ INNER JOIN
 	payment ON
 	staff.staff_id = payment.staff_id;
 
+#This Answers 6c
+SELECT film.title, film_actor.ACTOR_COUNT
+FROM film
+INNER JOIN
+	(SELECT film_id, COUNT(actor_id) AS ACTOR_COUNT FROM film_actor
+	GROUP BY film_id) 
+    film_actor ON
+    film.film_id = film_actor.film_id;
+    
+#This answers 6d
+SELECT film.film_id, film.title, inventory.MYCOUNT
+FROM film
+INNER JOIN
+	(SELECT film_id, COUNT(store_id) AS MYCOUNT FROM inventory
+    GROUP BY film_id) 
+    inventory ON
+    film.film_id = inventory.film_id
+WHERE film.title = "Hunchback Impossible";
 
-SELECT customer.first_name, customer.last_name, payment.mySUM
-FROM customer
+#This answers 6e
+SELECT customer.first_name, customer.last_name, payment.CUSTOMER_PAYMENT
+FROM  customer
 INNER JOIN 
-	(SELECT customer_id, SUM(amount) AS mySUM FROM payment
-	GROUP BY customer_id) payment ON
-	customer.customer_id=payment.customer_id;
+	(SELECT customer_id, SUM(amount) AS CUSTOMER_PAYMENT FROM payment
+	GROUP BY customer_id)
+	payment ON
+	customer.customer_id = payment.customer_id
+ORDER BY last_name ASC;
+
+#This Answers 7a
+#Tried using LIKE "[QK]%" But it did not work
+SELECT * 
+FROM film
+WHERE title LIKE "q%" AND language_id = 1;
+
+SELECT * 
+FROM film
+WHERE title LIKE "k%" AND language_id = 1;
+
+
+
 
 
